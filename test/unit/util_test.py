117b1d137f6a362ace6a31feb384b4cef2b1f6a7
pereaasvq <158350782+pereaasvq@users.noreply.github.com>
2024-06-11 18:15:22 +0200

    Update util_test.py

--- test/unit/util_test.py
+++ test/unit/util_test.py
@@ -18,6 +18,30 @@ class TestUtil(unittest.TestCase):
 
     def test_convert_to_number_invalid_type(self):
         self.assertRaises(TypeError, util.convert_to_number, "")
+        self.assertRaises(TypeError, util.convert_to_number, "3.h")
         self.assertRaises(TypeError, util.convert_to_number, "s")
         self.assertRaises(TypeError, util.convert_to_number, None)
         self.assertRaises(TypeError, util.convert_to_number, object())
+
+    def test_InvalidConvertToNumber_correct_param(self):
+        self.assertEqual(4, util.InvalidConvertToNumber("4"))
+        self.assertEqual(0, util.InvalidConvertToNumber("0"))
+        self.assertEqual(0, util.InvalidConvertToNumber("-0"))
+        self.assertEqual(-1, util.InvalidConvertToNumber("-1"))
+        self.assertAlmostEqual(4.0, util.InvalidConvertToNumber("4.0"), delta=0.0000001)
+        self.assertAlmostEqual(0.0, util.InvalidConvertToNumber("0.0"), delta=0.0000001)
+        self.assertAlmostEqual(0.0, util.InvalidConvertToNumber("-0.0"), delta=0.0000001)
+        self.assertAlmostEqual(-1.0, util.InvalidConvertToNumber("-1.0"), delta=0.0000001)
+
+    def test_InvalidConvertToNumber_invalid_type(self):
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, "")
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, "3.h")
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, "s")
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, None)
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, object())
+
+    def test_validate_permissions(self):
+        self.assertTrue(util.validate_permissions("operation", "user1"))
+        self.assertFalse(util.validate_permissions("operation", "user2"))
+        self.assertFalse(util.validate_permissions("operation", "user3"))
+        

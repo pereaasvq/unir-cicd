f4035b2ea98bafa8b17bfbe5cfd111512c9fc275
pereaasvq <158350782+pereaasvq@users.noreply.github.com>
2024-06-11 18:14:43 +0200

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,8 +1,11 @@
 import unittest
+from unittest.mock import patch
 import pytest
 
-from app.calc import Calculator
+from app.calc import Calculator, InvalidPermissions
 
+def mocked_validation(*args, **kwargs):
+    return True
 
 @pytest.mark.unit
 class TestCalculate(unittest.TestCase):
@@ -15,10 +18,6 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))
 
-    def test_divide_method_returns_correct_result(self):
-        self.assertEqual(1, self.calc.divide(2, 2))
-        self.assertEqual(1.5, self.calc.divide(3, 2))
-
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
         self.assertRaises(TypeError, self.calc.add, 2, "2")
@@ -28,6 +27,38 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.add, object(), 2)
         self.assertRaises(TypeError, self.calc.add, 2, object())
 
+    def test_substract_method_returns_correct_result(self):
+        self.assertEqual(0, self.calc.substract(2, 2))
+        self.assertEqual(4, self.calc.substract(2, -2))
+        self.assertEqual(-4, self.calc.substract(-2, 2))
+        self.assertEqual(1, self.calc.substract(1, 0))
+
+    def test_substract_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.substract, "2", 2)
+        self.assertRaises(TypeError, self.calc.substract, 2, "2")
+        self.assertRaises(TypeError, self.calc.substract, "2", "2")
+
+    @patch('app.util.validate_permissions', side_effect=mocked_validation, create=True)
+    def test_multiply_method_returns_correct_result(self, _validate_permissions):
+        self.assertEqual(4, self.calc.multiply(2, 2))
+        self.assertEqual(0, self.calc.multiply(1, 0))
+        self.assertEqual(0, self.calc.multiply(-1, 0))
+        self.assertEqual(-2, self.calc.multiply(-1, 2))
+
+    @patch('app.util.validate_permissions', side_effect=mocked_validation, create=True)
+    def test_multiply_method_fails_with_nan_parameter(self, _validate_permissions):
+        self.assertRaises(TypeError, self.calc.multiply, "2", 2)
+        self.assertRaises(TypeError, self.calc.multiply, 2, "2")
+        self.assertRaises(TypeError, self.calc.multiply, "2", "2")
+
+    @patch('app.util.validate_permissions', side_effect=lambda *args, **kwargs: False, create=True)
+    def test_multiply_method_fails_with_invalid_permissions(self, _validate_permissions):
+        self.assertRaises(InvalidPermissions, self.calc.multiply, 2, 2)
+
+    def test_divide_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.divide(2, 2))
+        self.assertEqual(1.5, self.calc.divide(3, 2))
+    
     def test_divide_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
@@ -39,6 +70,43 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 0, 0)
         self.assertRaises(TypeError, self.calc.divide, "0", 0)
 
+    def test_power_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.power(2, 2))
+        self.assertEqual(1, self.calc.power(2, 0))
+        self.assertEqual(0.25, self.calc.power(2, -2))
+        self.assertEqual(27, self.calc.power(3, 3))
 
+    def test_power_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.power, "2", 2)
+        self.assertRaises(TypeError, self.calc.power, 2, "2")
+        self.assertRaises(TypeError, self.calc.power, "2", "2")
+    
+    def test_sqrt_method_returns_correct_result(self):
+        self.assertEqual(2, self.calc.sqrt(4))
+        self.assertEqual(3, self.calc.sqrt(9))
+        self.assertAlmostEqual(1.414213562, self.calc.sqrt(2), places=9)
+
+    def test_sqrt_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.sqrt, "2")
+        self.assertRaises(TypeError, self.calc.sqrt, object())
+
+    def test_sqrt_method_fails_with_negative_parameter(self):
+        self.assertRaises(TypeError, self.calc.sqrt, -1)
+        self.assertRaises(TypeError, self.calc.sqrt, -100)
+
+    def test_log10_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.log10(10))
+        self.assertEqual(2, self.calc.log10(100))
+        self.assertEqual(0, self.calc.log10(1))
+
+    def test_log10_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.log10, "2")
+        self.assertRaises(TypeError, self.calc.log10, object())
+
+    def test_log10_method_fails_with_non_positive_parameter(self):
+        self.assertRaises(TypeError, self.calc.log10, 0)
+        self.assertRaises(TypeError, self.calc.log10, -10)
+        self.assertRaises(TypeError, self.calc.log10, -100)
+    
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()

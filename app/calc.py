bef0c890b5191d77ed6d1856ae4baad7a3fa2110
pereaasvq <158350782+pereaasvq@users.noreply.github.com>
2024-06-11 18:10:10 +0200

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,3 +1,11 @@
+import math
+import app
+
+
+class InvalidPermissions(Exception):
+    pass
+
+
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
@@ -8,6 +16,9 @@ class Calculator:
         return x - y
 
     def multiply(self, x, y):
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
+            raise InvalidPermissions('User has no permissions')
+        
         self.check_types(x, y)
         return x * y
 
@@ -15,19 +26,34 @@ class Calculator:
         self.check_types(x, y)
         if y == 0:
             raise TypeError("Division by zero is not possible")
-
+        
         return x / y
 
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
 
-    def check_types(self, x, y):
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Parameters must be numbers")
+    def sqrt(self, x):
+        self.check_types(x)
+        if x < 0:
+            raise TypeError("Square root of a negative number is not possible")
+        return math.sqrt(x)
+    
+    def log10(self, x):
+        self.check_types(x)
+        if x <= 0:
+            raise TypeError("Logarithm only defined for positive numbers")
+        return math.log10(x)
 
+    def check_types(self, x, y=None):
+        if y is not None:
+            if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+                raise TypeError("Parameters must be numbers")
+        else:
+            if not isinstance(x, (int, float)):
+                raise TypeError("Parameter must be a number")    
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
-    result = calc.add(2, 2)
+    result = calc.add(2, 3)
     print(result)
